d62db4562f012a16a9298a947ee959ddd813f385
Abram Chavez <abram.chavez820@gmail.com>
2021-06-21 20:45:26 -0500

    for loop with array. append to HTML unsuccesful

--- client.js
+++ client.js
@@ -1,1 +1,18 @@
+$(document).ready(onReady);
+function onReady() {
+    console.log("Starting the code challenge!");
+}
+
 console.log('Here are all the available people:', people);
+for (let person of people) {
+    function person(){
+        $('#gameboard').append(`<div class="grid" id="${person.name}"><img src="<img src="https://github.com/christopher-black.png?size=250" alt="Profile image of Chris">)
+
+function randomNumber(min, max){
+    return Math.floor(Math.random() * (1 + max - min) + min)
+}
+
+function person() {
+    $(div).append('person')
+}
+
